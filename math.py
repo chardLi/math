07fbe7ee4c7ef4ca71555726c5ea44eeb92c0da6
chardLi <liy_1998@126.com>
2019-07-02 20:08:10 +0800

    auto update

--- math.py
+++ math.py
@@ -1,11 +1,12 @@
 from urllib import  request
 from bs4 import BeautifulSoup
 import   os
+import codecs
 
 template='''<!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="gbk" />
+	<meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
    
@@ -423,7 +424,7 @@ if __name__ == '__main__':
     printimg(img_a,imgtit)
 
     print(template)
-    file = open(r'./docs/静态网页第一版/index.html','w')
+    file = codecs.open(r'./docs/静态网页第一版/index.html','w',encoding='utf-8')
     file.write(template)
     file.close()
     os.system(r'git commit -am "auto update"')
